adc0ab6f7d086e44f8b8fa14fcff2f178233e897
Christopher K. <info@kitsoft.at>
2023-07-07 22:43:22 +0200

    Registered ReadyClass, fixed wrong log file location.

--- LGC_PublicBot/src/eu/lotusgc/bot_public/main/Main.java
+++ LGC_PublicBot/src/eu/lotusgc/bot_public/main/Main.java
@@ -14,12 +14,14 @@ import java.util.logging.SimpleFormatter;
 
 import org.simpleyaml.configuration.file.YamlFile;
 
+import eu.lotusgc.bot_public.commands.PublicGuildCommands;
+import eu.lotusgc.bot_public.event.ReadyClass;
 import eu.lotusgc.bot_public.misc.MySQL;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
-public class Main {
+public class Main{
 	
 	public static String configFolderName = "LotusPublicBot";
 	public static File configurationFolder = new File(configFolderName);
@@ -52,6 +54,8 @@ public class Main {
 		JDABuilder builder = JDABuilder.createDefault(cfg.getString("Bot.token"));
 		builder.enableIntents(GatewayIntent.GUILD_MODERATION, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_VOICE_STATES);
 		builder.enableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS, CacheFlag.EMOJI, CacheFlag.FORUM_TAGS, CacheFlag.ONLINE_STATUS, CacheFlag.SCHEDULED_EVENTS, CacheFlag.VOICE_STATE);
+		builder.addEventListeners(new PublicGuildCommands());
+		builder.addEventListeners(new ReadyClass());
 		builder.build();
 	}
 	
@@ -102,7 +106,7 @@ public class Main {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd-HH_mm_ss");
 		String date = sdf.format(new Date());
 		try {
-			FileHandler fileHandler = new FileHandler("log-" + date + ".log.txt");
+			FileHandler fileHandler = new FileHandler(configFolderName + "/logs/log-" + date + ".log.txt");
 			fileHandler.setFormatter(new SimpleFormatter());
 			
 			ConsoleHandler consoleHandler = new ConsoleHandler();
