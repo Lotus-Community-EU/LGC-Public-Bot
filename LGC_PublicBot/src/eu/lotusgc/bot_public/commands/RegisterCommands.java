a3de796cf27c9c6c8c162c520dcd482835826361
Christopher Wilhelm H. <info@kitsoft.at>
2024-03-24 03:16:50 +0100

    Added Public Voice Commands

--- LGC_PublicBot/src/eu/lotusgc/bot_public/commands/RegisterCommands.java
+++ LGC_PublicBot/src/eu/lotusgc/bot_public/commands/RegisterCommands.java
@@ -2,7 +2,9 @@ package eu.lotusgc.bot_public.commands;
 
 import eu.lotusgc.bot_public.main.Main;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
@@ -19,7 +21,23 @@ public class RegisterCommands {
 				Commands.slash("guildinfo", "View guild related infos like roles, users and such."),
 				Commands.slash("user", "See the user from our Minecraft Playerdatabase."),
 				Commands.slash("serverinfo", "View detailed Informations about a game server."),
-				Commands.slash("ticket", "Need help? Create a ticket!")
+				Commands.slash("ticket", "Need help? Create a ticket!"),
+				
+				//Private voice 
+				Commands.slash("adminvoice-category", "Administration for 'voice-channel'.")
+				.addOption(OptionType.CHANNEL, "category", "The category to set as default", true)
+				.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL)),
+				Commands.slash("adminvoice-settings", "Setup the private voices.")
+				.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL)),
+				Commands.slash("voice-create", "Create a voice channel")
+				.addOption(OptionType.BOOLEAN, "private", "Whether the channel should be open or not. (true=private, false=public)", true)
+				.addOption(OptionType.INTEGER, "slots", "How many slots should the voice channel have?"),
+				Commands.slash("voice-delete", "Deletes the voice channel"),
+				Commands.slash("voice-permit", "Adds a person to the channel permissions (just for private channels!)")
+				.addOption(OptionType.USER, "user", "The user to add", true),
+				Commands.slash("voice-revoke", "Removes a person from the channel permissions (just for private channels!)")
+				.addOption(OptionType.USER, "user", "The user to remove", true)
+				.addOption(OptionType.BOOLEAN, "remove", "Whether to forcefully remove the user from the voice or not.")
 		).queue();
 			Main.logger.info("Registered commands on 'PublicGuildCommands' for " + guild.getName() + " guilds.");
 		}
